d122026aa572acc33cd6dae32af606b4e2196bd9
Filip Komarzyniec <filip.komarzyniec@ibm.com>
2026-02-09 10:31:14 +0100

    components simplified and changed to python lightweight ones

--- components/training/autorag/rag_templates_optimization/component.py
+++ components/training/autorag/rag_templates_optimization/component.py
@@ -21,7 +21,7 @@ from components.training.autorag.rag_templates_optimization.src.utils import (
 )
 
 
-@dsl.component(target_image="rag_base:test")
+@dsl.component(base_image="rag_base:test")
 def rag_templates_optimization(
     extracted_text: dsl.InputPath(dsl.Artifact),
     test_data: dsl.InputPath(dsl.Artifact),
@@ -62,8 +62,83 @@ def rag_templates_optimization(
 
     """
 
+    import os
+    from json import dump as json_dump
+    from pathlib import Path
+    from random import random
+    from typing import Any, List, TextIO
+
+    import pandas as pd
+    import yaml as yml
+    from ai4rag.core.experiment.experiment import AI4RAGExperiment
+    from ai4rag.core.hpo.base_optimiser import OptimiserSettings
+    from ai4rag.rag.embedding.base_model import EmbeddingModel
+    from ai4rag.rag.foundation_models.base_model import FoundationModel
+    from ai4rag.search_space.src.parameter import Parameter
+    from ai4rag.search_space.src.search_space import AI4RAGSearchSpace
+    from event_handlers import TmpEventHandler
+    from langchain_core.documents import Document
+    from proxy_objects import DisconnectedAI4RAGExperiment, StdoutEventHandler
+
     MAX_NUMBER_OF_RAG_PATTERNS = 8
     METRIC = "faithfulness"
+
+    def load_as_langchain_doc(path: str | Path) -> list[Document]:
+        """
+        Given path to a text-based file or a folder thereof load everything to memory and
+        return as a list of langchain `Document` objects.
+
+        Args:
+            path
+                A local path to either a text file or a folder of text files.
+        Returns:
+            A list of langchain `Document` objects.
+
+        Note:
+
+        """
+
+        if isinstance(path, str):
+            path = Path(path)
+
+        documents = []
+        if path.is_dir():
+            for doc_path in path.iterdir():
+                with doc_path.open("r", encoding="utf-8") as doc:
+                    documents.append(Document(page_content=doc.read(), metadata={"file_name": doc_path.name}))
+
+        elif path.is_file():
+            with path.open("r", encoding="utf-8") as doc:
+                documents.append(Document(page_content=doc.read(), metadata={"file_name": path.name}))
+
+        return documents
+
+    class MockGenerationModel(FoundationModel):
+
+        def __init__(
+            self,
+            model_id: str,
+            client: None = None,
+            model_params: dict[str, Any] | None = None,
+        ):
+            super().__init__(client, model_id, model_params)
+
+        def chat(self, system_message: str, user_message: str) -> str:
+            return "Dummy response from a generation model!"
+
+    class MockEmbeddingModel(EmbeddingModel):
+        def __init__(self, model_id: str, params: dict[str, Any] | None = None, client: None = None):
+            super().__init__(client, model_id, params)
+
+        def embed_documents(self, texts: List[str]) -> List[List[float]]:
+            n = []
+            for _ in texts:
+                n.append([random() for _ in range(self.params["embedding_dimension"])])
+
+            return n
+
+        def embed_query(self, query: str) -> List[float]:
+            return [random() for _ in range(self.params["embedding_dimension"])]
 
     optimization_settings = optimization_settings if optimization_settings else {}
     if not (optimization_metric := optimization_settings.get("metric", None)):
@@ -72,7 +147,17 @@ def rag_templates_optimization(
     documents = load_as_langchain_doc(extracted_text)
 
     # recreate the search space
-    search_space = load_search_space_from(search_space_prep_report)
+    with open(search_space_prep_report, "r") as f:
+        search_space = yml.load(f, yml.SafeLoader)
+    params = []
+    for param, values in search_space.items():
+        if param == "foundation_models":
+            params.append(Parameter("foundation_model", "C", values=list(map(MockGenerationModel, values))))
+        elif param == "embedding_models":
+            params.append(Parameter("embedding_model", "C", values=list(map(MockEmbeddingModel, values))))
+        else:
+            params.append(Parameter(param, "C", values=values))
+    search_space = AI4RAGSearchSpace(params=params)
 
     # TODO chunking should be handled externally
     # OR
@@ -83,7 +168,8 @@ def rag_templates_optimization(
     optimiser_settings = OptimiserSettings(
         max_evals=optimization_settings.get("max_number_of_rag_patterns", MAX_NUMBER_OF_RAG_PATTERNS)
     )
-    benchmark_data = pd.read_json(test_data)
+
+    benchmark_data = pd.read_json(Path(test_data))
 
     # TODO exact naming of the secret is yet to be defined
     client_connection = os.environ.get("LLAMASTACK_CLIENT_CONNECTION", None)
@@ -110,15 +196,15 @@ def rag_templates_optimization(
 
     for eval in rag_exp.results.evaluations:
         patt_dir = rag_patterns_dir / eval.pattern_name
-        patt_dir.mkdir()
-        with (patt_dir / "pattern.json").open("w") as pattern_details:
+        patt_dir.mkdir(parents=True, exist_ok=True)
+        with (patt_dir / "pattern.json").open("w+") as pattern_details:
             # json_dump(rag_exp._stream_finished_pattern(eval, []), pattern_details)
             json_dump(eval.to_dict(), pattern_details)
 
-        with (patt_dir / "inference_notebook.ipynb").open("w") as inf_notebook:
+        with (patt_dir / "inference_notebook.ipynb").open("w+") as inf_notebook:
             json_dump({"inference_notebook_cell": "cell_value"}, inf_notebook)
 
-        with (patt_dir / "indexing_notebook.ipynb").open("w") as ind_notebook:
+        with (patt_dir / "indexing_notebook.ipynb").open("w+") as ind_notebook:
             json_dump({"ind_notebook_cell": "cell_Value"}, ind_notebook)
 
     # TODO leaderboard artifact
--- components/training/autorag/search_space_preparation/component.py
+++ components/training/autorag/search_space_preparation/component.py
@@ -1,25 +1,37 @@
 import os
-from typing import Optional
+from pathlib import Path
+from random import random
+from typing import Any, Dict, List, Optional
 
 import pandas as pd
 import yaml as yml
 from ai4rag.core.experiment.benchmark_data import BenchmarkData
 from ai4rag.core.experiment.mps import ModelsPreSelector
+from ai4rag.rag.embedding.base_model import EmbeddingModel
+from ai4rag.rag.foundation_models.base_model import FoundationModel
 from ai4rag.search_space.prepare.prepare_search_space import prepare_ai4rag_search_space
+from ai4rag.search_space.src.parameter import Parameter
+from ai4rag.search_space.src.search_space import AI4RAGSearchSpace
 from kfp import dsl
+from langchain_core.documents import Document
 
 from components.training.autorag.rag_templates_optimization.src.utils import load_as_langchain_doc
 from components.training.autorag.search_space_preparation.proxy_objects import DisconnectedModelsPreSelector
 
 
-@dsl.component(target_image="rag_base:test")
+@dsl.component(base_image="rag_base:test")
 def search_space_preparation(
-    test_data: dsl.InputPath(dsl.Artifact),
-    extracted_text: dsl.InputPath(dsl.Artifact),
-    search_space_prep_report: dsl.OutputPath(dsl.Artifact),
-    constraints: Optional[dict] = None,
-    models_config: Optional[dict] = None,  # ???
-    metric: Optional[str] = None,
+    test_data: dsl.Input[dsl.Artifact],
+    extracted_text: dsl.Input[dsl.Artifact],
+    search_space_prep_report: dsl.Output[dsl.Artifact],
+    # test_data: dsl.InputPath(dsl.Artifact),
+    # extracted_text: dsl.InputPath(dsl.Artifact),
+    # search_space_prep_report: dsl.OutputPath(dsl.Artifact),
+    # constraints: Dict = None,
+    embeddings_models: Optional[List] = None,
+    generation_models: Optional[List] = None,
+    models_config: Dict = None,  # ???
+    metric: str = None,
 ):
     """
     Runs an AutoRAG experiment's first phase which includes:
@@ -49,25 +61,118 @@ def search_space_preparation(
             A .yml-formatted report including results of this experiment's phase.
             For its exact content please refer to the `search_space_prep_report_schema.yml` file.
     """
+
+    import os
+    from pathlib import Path
+    from random import random
+    from typing import Any, Optional
+
+    import pandas as pd
+    import yaml as yml
+    from ai4rag.core.experiment.benchmark_data import BenchmarkData
+    from ai4rag.core.experiment.mps import ModelsPreSelector
+    from ai4rag.rag.embedding.base_model import EmbeddingModel
+    from ai4rag.rag.foundation_models.base_model import FoundationModel
+    from ai4rag.search_space.prepare.prepare_search_space import prepare_ai4rag_search_space
+    from ai4rag.search_space.src.parameter import Parameter
+    from ai4rag.search_space.src.search_space import AI4RAGSearchSpace
+    from kfp import dsl
+    from langchain_core.documents import Document
+    from proxy_objects import DisconnectedModelsPreSelector
+    from utils import load_as_langchain_doc
+
     # TODO whole component has to be run conditionally
     # TODO these defaults should be exposed by ai4rag library
-    Top_N_FM = 3  # change names (topNmodels? )
-    Top_K_EM = 2
+    TOP_N_GENERATION_MODELS = 3  # change names (topNmodels? )
+    TOP_K_EMBEDDING_MODELS = 2
     METRIC = "faithfulness"
     SAMPLE_SIZE = 5
     SEED = 17
 
-    # build search space
-    constraints = constraints if constraints else {}
-    search_space = prepare_ai4rag_search_space(constraints)
+    class MockGenerationModel(FoundationModel):
 
-    benchmark_data = BenchmarkData(pd.read_json(test_data))
-    documents = load_as_langchain_doc(extracted_text)
+        def __init__(
+            self,
+            model_id: str,
+            client: None = None,
+            model_params: dict[str, Any] | None = None,
+        ):
+            super().__init__(client, model_id, model_params)
+
+        def chat(self, system_message: str, user_message: str) -> str:
+            return "Dummy response from a generation model!"
+
+    class MockEmbeddingModel(EmbeddingModel):
+        def __init__(self, model_id: str, params: dict[str, Any] | None = None, client: None = None):
+            super().__init__(client, model_id, params)
+
+        def embed_documents(self, texts: List[str]) -> List[List[float]]:
+            n = []
+            for _ in texts:
+                n.append([random() for _ in range(self.params["embedding_dimension"])])
+
+            return n
+
+        def embed_query(self, query: str) -> List[float]:
+            return [random() for _ in range(self.params["embedding_dimension"])]
+
+    def load_as_langchain_doc(path: str | Path) -> list[Document]:
+        """
+        Given path to a text-based file or a folder thereof load everything to memory and
+        return as a list of langchain `Document` objects.
+
+        Args:
+            path
+                A local path to either a text file or a folder of text files.
+        Returns:
+            A list of langchain `Document` objects.
+
+        Note:
+
+        """
+
+        if isinstance(path, str):
+            path = Path(path)
+
+        documents = []
+        if path.is_dir():
+            for doc_path in path.iterdir():
+                with doc_path.open("r", encoding="utf-8") as doc:
+                    documents.append(Document(page_content=doc.read(), metadata={"file_name": doc_path.name}))
+
+        elif path.is_file():
+            with path.open("r", encoding="utf-8") as doc:
+                documents.append(Document(page_content=doc.read(), metadata={"file_name": path.name}))
+
+        return documents
+
+    def prepare_ai4rag_search_space():
+        if not generation_models:
+            generation_models_local = ["mocked_gen_model1"]
+        if not embeddings_models:
+            embedding_models_local = ["mocked_em_model1"]
+
+        generation_models_local = list(map(MockGenerationModel, generation_models))
+        embedding_models_local = list(map(MockEmbeddingModel, embeddings_models))
+
+        return AI4RAGSearchSpace(
+            params=[
+                Parameter("foundation_model", "C", values=generation_models_local),
+                Parameter("embedding_model", "C", values=embedding_models_local),
+            ]
+        )
+
+    # build search space
+    # constraints = constraints if constraints else {}
+    search_space = prepare_ai4rag_search_space()
+
+    benchmark_data = BenchmarkData(pd.read_json(Path(test_data.path)))
+    documents = load_as_langchain_doc(extracted_text.path)
 
     mps = ModelsPreSelector(
         benchmark_data=benchmark_data.get_random_sample(n_records=SAMPLE_SIZE, random_seed=SEED),
         documents=documents,
-        foundation_models=search_space._search_space["inference_model_id"].values,
+        foundation_models=search_space._search_space["foundation_model"].values,
         embedding_models=search_space._search_space["embedding_model"].values,
         metric=metric if metric else METRIC,
     )
@@ -78,17 +183,18 @@ def search_space_preparation(
 
     mps.evaluate_patterns()
 
-    selected_models = mps.select_models(n_em=Top_K_EM, n_fm=Top_N_FM)
+    selected_models = mps.select_models(n_em=TOP_K_EMBEDDING_MODELS, n_fm=TOP_N_GENERATION_MODELS)
+    selected_models_names = {k: list(map(str, v)) for k, v in selected_models.items()}
 
     verbose_search_space_repr = {
         k: v.all_values()
         for k, v in search_space._search_space.items()
-        if k not in ("inference_model_id", "embedding_model")
+        if k not in ("foundation_model", "embedding_model")
     }
-    verbose_search_space_repr |= selected_models
+    verbose_search_space_repr |= selected_models_names
 
-    with open(search_space_prep_report, "w") as report_file:
-        yml.dump(verbose_search_space_repr, report_file)
+    with open(search_space_prep_report.path, "w") as report_file:
+        yml.dump(verbose_search_space_repr, report_file, yml.SafeDumper)
 
 
 if __name__ == "__main__":
