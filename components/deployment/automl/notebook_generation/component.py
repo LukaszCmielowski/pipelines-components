c08d3aa8762d6bb10519867a9bcc642ebb7c66a3
Dorota Laczak <34707786+DorotaDR@users.noreply.github.com>
2026-02-19 14:02:13 +0100

    fix: Reduced package installation for AutoML's training components. (#21)
    * fix: Reduced package installation for AutoML's training components.
    
    * fix: Linter fixes
    
    * fix: Downgrade the fastai version due to issue in fastai ImportError.
    
    Limited the fastai version due to https://github.com/autogluon/autogluon/issues/5521

--- components/deployment/automl/notebook_generation/component.py
+++ components/deployment/automl/notebook_generation/component.py
@@ -113,7 +113,14 @@ def notebook_generation(
                 "id": "cec84205-8ee9-4aaf-a97e-4ef576e7b9da",
                 "metadata": {},
                 "outputs": [],
-                "source": ["%pip install autogluon.tabular[all]==1.5 | tail -n 1"],
+                "source": [
+                    "%pip install autogluon.tabular==1.5.0 | tail -n 1\n",
+                    "%pip install catboost==1.2.8 | tail -n 1\n",
+                    "%pip install fastai==2.8.5 | tail -n 1\n",
+                    "%pip install lightgbm==4.6.0 | tail -n 1\n",
+                    "%pip install torch==2.9.1 | tail -n 1\n",
+                    "%pip install xgboost==3.1.3 | tail -n 1\n",
+                ],
             },
             {
                 "cell_type": "markdown",
@@ -409,7 +416,14 @@ def notebook_generation(
                 "id": "cec84205-8ee9-4aaf-a97e-4ef576e7b9da",
                 "metadata": {},
                 "outputs": [],
-                "source": ["%pip install autogluon.tabular[all]==1.5 | tail -n 1"],
+                "source": [
+                    "%pip install autogluon.tabular==1.5.0 | tail -n 1\n",
+                    "%pip install catboost==1.2.8 | tail -n 1\n",
+                    "%pip install fastai==2.8.5 | tail -n 1\n",
+                    "%pip install lightgbm==4.6.0 | tail -n 1\n",
+                    "%pip install torch==2.9.1 | tail -n 1\n",
+                    "%pip install xgboost==3.1.3 | tail -n 1\n",
+                ],
             },
             {
                 "cell_type": "markdown",
