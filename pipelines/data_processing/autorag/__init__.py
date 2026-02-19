b0b7f8b86666944a4894639646f6f287a62cfc57
Witold Nowogorski <wnowogor@redhat.com>
2026-02-19 14:08:32 +0100

    update pyproject.toml

---
+++ pipelines/data_processing/autorag/__init__.py
@@ -0,0 +1,3 @@
+from pipelines.data_processing.autorag.pipeline import data_processing_pipeline
+
+__all__ = ["data_processing_pipeline"]
--- pipelines/data_processing/autorag/data_processing_pipeline/__init__.py
+++
@@ -1,3 +0,0 @@
-from pipelines.data_processing.autorag.data_processing_pipeline.pipeline import data_processing_pipeline
-
-__all__ = ["data_processing_pipeline"]
